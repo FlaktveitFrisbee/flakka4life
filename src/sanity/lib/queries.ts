19416d95c55756ad29797346c5fc075e6e36ed4e
Jostein Tysse <jostein.h.tysse@gmail.com>
2025-04-13 13:58:17 +0200

    Fix order of operations

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -3,7 +3,7 @@
 import { defineQuery } from "next-sanity";
 
 export const POSTS_QUERY = defineQuery(
-  `*[_type == "post" && defined(slug.current)][0...12] | order(publishedAt desc) {
+  `*[_type == "post" && defined(slug.current)] | order(publishedAt desc)[0...12] {
   title,
   slug,
   publishedAt,
