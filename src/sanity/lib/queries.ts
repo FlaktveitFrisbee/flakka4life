7d9bdd8abb6e8126f80dfda135b34e74c1592d05
Jostein Tysse <jostein.h.tysse@gmail.com>
2025-04-13 13:56:09 +0200

    Sort posts by publishdate

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -3,7 +3,7 @@
 import { defineQuery } from "next-sanity";
 
 export const POSTS_QUERY = defineQuery(
-  `*[_type == "post" && defined(slug.current)][0...12] {
+  `*[_type == "post" && defined(slug.current)][0...12] | order(publishedAt desc) {
   title,
   slug,
   publishedAt,
